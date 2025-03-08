9b3eda146909e3288b2acc66dae7e166eacfdaba
Christopher Farrell <chris.farrell@email.com>
2025-03-08 11:17:37 +1000

    Add DualPaneExplorer/Program.cs 1741396656857

---
+++ DualPaneExplorer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace DualPaneExplorer;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
